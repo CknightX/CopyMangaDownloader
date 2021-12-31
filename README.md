ee843c8b202d39133972ee24a2e834da0458efb5
CknightX <caokeck123@163.com>
2021-12-31 12:42:48 +0800

    增加追漫功能

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # CopyMangaDownloader
-
-A simple CopyManga Downloader based on asyncio.
+CopyManga网漫画下载器，基于asyncio实现高速异步下载。
+## 使用
+1. 安装依赖 `pip install -r requirements.txt`
+2. 运行 `python downloader.py`
+## 功能
+### 漫画下载
+输入对应的pathword与章节名即可下载漫画。
+pathword是漫画页面url中comic后面的一串字符，章节名要和**官网上给出的章节名一致**。章节名支持范围输入，如：第1话-第10话，使用“-”隔开。
+漫画会被下载到工作路径下以官网漫画名命名的文件夹中，每一话也是独立的文件夹存放。
+### 追漫
+同目录下新建`watching.json`，内容如下：
+```json
+{
+    "pathword" : "最新已看章节"
+}
+```
+如：
+```json
+{
+    "zongzhijiushifeichangkeaiflymetothemoon" : "第174话",
+    "xxx" : "第xxx话"
+}
+```
+脚本中提供选项对`watching.json`中的所有漫画进行更新检查，并把未看的新章节自动下载到本地。
+## 其他
+此脚本是个人在线看漫画过程中因有时加载太慢影响观看体验而编写，主要用于追漫更新。请不要大批量爬取漫画，给网站造成过大压力，谢谢。
--- downloader.py
+++ downloader.py
@@ -1,4 +1,7 @@
 import asyncio,aiohttp,aiofiles
+from asyncio.tasks import ensure_future
+from json import encoder
+from json.decoder import JSONDecodeError
 import json,os,logging,re
 logging.basicConfig(level = logging.INFO,format = '[%(levelname)s] %(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -14,8 +17,7 @@ COMIC_INDEX = 'https://www.copymanga.com/comic/{pathword}'
 
 retry = 3 # 下载失败重试次数
 
-g_chapters = None
-g_pathword = None
+g_chapters = {} # 章节缓存
 g_download_failed = []
 
 async def download(url,path):
@@ -41,8 +43,13 @@ async def download(url,path):
         logger.error(f'{path} download failed.')
 
 async def fetch(session, url):
-    async with session.get(url) as resp:
-        return await resp.text()
+    for i in range(retry):
+        try:
+            async with session.get(url) as resp:
+                return await resp.text()
+        except:
+            continue
+    
 
 async def get_title_by_pathword(pathword):
     async with aiohttp.ClientSession(headers=headers) as session:
@@ -57,26 +64,28 @@ async def get_chapters_uuid(pathword):
         pathword: /comic/<pathword>/
         raw:      是否返回原始list
     Returns:
-        {chapter_name : uuid}
+        [(chapter_name, uuid)]
     """
-    global g_chapters,g_pathword
-    if g_chapters is not None and pathword == g_pathword:
-        return g_chapters
+    global g_chapters
+    if pathword in g_chapters:
+        return g_chapters[pathword]
+
     async with aiohttp.ClientSession(headers=headers) as session:
         json_src = await fetch(session,API_CHAPTER.format(pathword=pathword))
     json_obj = json.loads(json_src)
     chapters = json_obj['results']['list'] # name uuid
     chapters = list(map(lambda x:(x['name'],x['uuid']),chapters))
-    g_pathword = pathword
-    g_chapters = chapters
+    g_chapters[pathword] = chapters
     return chapters
 
-async def get_chapter_uuid(pathword,chapter:str):
+async def get_chapter_uuid(pathword,chapter:str) -> str:
     """根据漫画和章节名获取章节uuid
     Args:
+        pathword:     漫画的识别串
         chapter: "第x话"
     Returns:
-        chapter对应的uuid
+        成功，返回chapter对应的uuid
+        失败，返回''
     """
     chapters = await get_chapters_uuid(pathword)
     if chapter not in chapters:
@@ -87,12 +96,13 @@ async def get_chapter_uuid(pathword,chapter:str):
             return chapter[1]
     return ''
 
-async def get_pics_url(pathword,chapter_uuid:str):
-    """根据章节的uuid获取所有图片的url
+async def get_pics_url(pathword,chapter_uuid:str) -> list[str]:
+    """根据pathword,章节的uuid获取所有图片的url
     Args:
+        pathword:     漫画的识别串
         chapter_uuid: 章节的uuid
     Returns:
-        list(图url)
+        list(url)
     """
     async with aiohttp.ClientSession(headers=headers) as session:
         json_src = await fetch(session,API_COMIC.format(pathword=pathword,uuid=chapter_uuid))
@@ -107,6 +117,8 @@ async def download_chapter(pathword,chapter,chapter_uuid,manga_name=''):
         manga_name: 漫画名，用于本地文件夹的命名。如果保持空则自动获取
         chapter_uuid: 可省略为'' 如果省略则通过get_chapter_uuid获取
     """
+    if pathword == '' or chapter == '':
+        return
     if manga_name == '':
         manga_name = await get_title_by_pathword(pathword)
     folder = os.path.join(manga_name,chapter)
@@ -126,6 +138,8 @@ async def download_chapters(pathword,chapter_range:str,manga_name=''):
     Args:
         chapters: 第xx话-第xx话
     """
+    if pathword == '' or chapter_range == '':
+        return
     tasks = []
     download_flag = 0
     if manga_name == '':
@@ -149,23 +163,83 @@ async def download_chapters(pathword,chapter_range:str,manga_name=''):
             download_flag = 2
     await asyncio.gather(*tasks)
 
+async def get_all_updated_manga():
+    """获取追漫列表最新更新
+        Returns:
+            [(pathword:str,chapter_range:str)]
+    """
+    async def get_updated(pathword,curr):
+        chapters = await get_chapters_uuid(pathword)
+        bg = -1
+        for i,chapter in enumerate(chapters):
+            if chapter[0] == curr:
+                bg = i + 1
+        if bg == len(chapters):
+            return (pathword,'')
+        return (pathword,f'{chapters[bg][0]}-{chapters[-1][0]}')
+    data = []
+    try:
+        with open('./watching.json','r',encoding='utf-8') as f:
+            data = json.loads(f.read())
+    except JSONDecodeError as e:
+        logger.error('Read data.json error.')
+    res = []
+    for name in data:
+        updated = await get_updated(name,data[name])
+        if updated[1] == '':
+            continue
+        res.append(updated)
+    return res
+
+async def updated_watching():
+    updated = await get_all_updated_manga()
+    if len(updated) == 0:
+        logger.info('No updated.')
+    else:
+        tasks = []
+        with open('./watching.json','r',encoding='utf-8') as f:
+            data = json.loads(f.read())
+        for per in updated:
+            tasks.append(asyncio.create_task(download_chapters(per[0],per[1])))
+        await asyncio.gather(*tasks)
+        for per in updated:
+            data[per[0]] = per[1].split('-')[1]
+        with open('./watching.json','w',encoding='utf-8') as f:
+            f.write(json.dumps(data,indent=4,ensure_ascii=False))
+
+async def re_download_failed():
+    global g_download_failed
+    tmp = g_download_failed
+    g_download_failed = []
+    for per in tmp:
+        await download(per[0],per[1])
+
 def main():
     global g_download_failed
     print("""
     ----------------------------
      CopyMangaDownloader  by:ck
+     1. 更新追漫列表
+     2. 下载漫画
+     3. 重新下载失败列表
     ----------------------------
     """)
     loop = asyncio.get_event_loop()
     while True:
-        g_download_failed = []
-        pathword = input('Pathword: ')
-        chapter = input('Chapter: ')
-        try:
-            loop.run_until_complete(download_chapters(pathword,chapter))
-        except Exception as e:
-            logger.error(e)
-        logger.info('Download finished.')
+        mode = input('You want?[1/2/3]: ')
+        if mode == '1':
+            loop.run_until_complete(updated_watching())
+        elif mode == '2':
+            pathword = input('Pathword: ')
+            chapter = input('Chapter: ')
+            try:
+                loop.run_until_complete(download_chapters(pathword,chapter))
+            except Exception as e:
+                logger.error(e)
+            logger.info('Download finished.')
+        elif mode == '3':
+            loop.run_until_complete(re_download_failed())
+        # 下载失败列表
         if len(g_download_failed) > 0:
             logger.warning(f'Failed list:{g_download_failed}')
         print('----------------------------')
